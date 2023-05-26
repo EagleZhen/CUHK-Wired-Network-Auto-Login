b356a684fd19aee5f5a5aeeac51a65cf9308853a
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2023-05-27 05:04:57 +0800

    testing windows 11 system tray

--- function testing/4. system tray.py
+++ function testing/4. system tray.py
@@ -6,18 +6,11 @@ from ez import pause
 import sys
 
 if sys.platform == "win32":
-    # Windows-specific code
-    # Include code for displaying notifications on Windows
-	from win11toast import ToastNotifier
+    # Windows-specific
+	from win11toast import toast
 else:
 	# Umm, I am not familiar with other platforms
 	pass
-
-def show_notification():
-	toaster = ToastNotifier()
-	toaster.show_toast("Notification Title", "Notification Message", duration=5)
-
-menu_options = (("Show Notification", None, show_notification),)
 
 def hello(systray):
 	print("Hello!")
@@ -25,14 +18,13 @@ def hello(systray):
 # Load the tray icon image using relative path to the script directory
 script_dir = dirname(abspath(__file__))
 icon_path = join(script_dir, "../icon/icon.ico")
+pause (icon_path)
 
 # Create the tray icon menu, each bracket for one menu option
 	# "Hello": text label for the option
 	# None: icon for the option
 	# hello: callback function for the option
 menu_options = (("Hello", None, hello),)
-
-pause (icon_path)
 
 # # Create the tray icon object
 systray_options = {
