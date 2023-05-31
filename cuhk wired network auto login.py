71783df0b144f006f799c481188e4dd2e0c2cd82
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2023-05-31 17:13:48 +0800

    added expiry time prediction (require authentication again every 12 hours if I remember correctly)

--- cuhk wired network auto login.py
+++ cuhk wired network auto login.py
@@ -2,16 +2,16 @@ import requests
 import json
 import socket
 from time import sleep
-from datetime import datetime
+from datetime import datetime,timedelta
 
 def is_internet_connected():
-    try:
-        # Attempt to establish a connection to the Google DNS server (8.8.8.8) on port 53, with a timeout of 3 seconds
-        socket.create_connection(("8.8.8.8", 53), timeout=3)
-        return True
-    except OSError:
-        pass
-    return False
+	try:
+		# Attempt to establish a connection to the Google DNS server (8.8.8.8) on port 53, with a timeout of 3 seconds
+		socket.create_connection(("8.8.8.8", 53), timeout=3)
+		return True
+	except OSError:
+		pass
+	return False
 
 def print_message(message):
 	# Show the timestamps for the corresponding status code
@@ -20,35 +20,42 @@ def print_message(message):
 	# Print timestamps with the message
 	print(formatted_datetime,"|",message)
 
-login_url = "https://securelogin.net.cuhk.edu.hk/cgi-bin/login"
+def get_expiry_time():
+	expiry_time = datetime.now() + timedelta(hours=12)
 
-# Load the credentials from the json file
-with open("credential/credential.json") as f:
-	credentials = json.load(f)
+	expiry_time_str = expiry_time.strftime("%Y-%m-%d %H:%M:%S")
+	return expiry_time_str
 
-form_data = {
-	"user": credentials["username"],
-	"password": credentials["password"],
-	"cmd": "authenticate"
-}
+if __name__ == "__main__":
+	login_url = "https://securelogin.net.cuhk.edu.hk/cgi-bin/login"
 
-# Keep monitoring the network status with an interval of 5 seconds
-interval = 5
-print_message("Network monitoring started...")
-while True:
-	if (is_internet_connected()==False):
-		print_message("Network disconnected. Trying to reconnect...")
+	# Load the credentials from the json file
+	with open("credential/credential.json") as f:
+		credentials = json.load(f)
 
-		while (is_internet_connected()==False):
-			# Send the POST request
-			requests.post(login_url, data=form_data)
+	form_data = {
+		"user": credentials["username"],
+		"password": credentials["password"],
+		"cmd": "authenticate"
+	}
 
-			if (is_internet_connected()==True):
-				print_message("Login successful!")
-			else:
-				print_message(f"Login failed. Trying to reconnect after a few seconds...")
+	# Keep monitoring the network status with an interval of 5 seconds
+	interval = 5
+	print_message("Network monitoring started...")
+	while True:
+		if (is_internet_connected()==False):
+			print_message(f"Network disconnected. Trying to reconnect as <{form_data['user']}>...")
 
-			sleep(interval)
+			while (is_internet_connected()==False):
+				# Send the POST request
+				requests.post(login_url, data=form_data)
 
-	# Break between every netowrk status checking
-	sleep(interval)
+				if (is_internet_connected()==True):
+					print_message(f"Login successful! It will expire at around {get_expiry_time()}")
+				else:
+					print_message(f"Login failed. Trying to reconnect after a few seconds...")
+
+				sleep(interval)
+
+		# Break between every netowrk status checking
+		sleep(interval)
